611d6913cf40764ab0c6b27aaac64b8eaaaccab3
duyanhitbe <duyanh.it.work@gmail.com>
2022-04-13 09:30:55 +0700

    cluster

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,19 @@ import bodyParser from "body-parser";
 import express from "express";
 import { PORT } from "./constants";
 import router from "./routes/index.routes";
+import cluster from "cluster";
 
 const app = express();
+const cpu_length = require("os").cpus().length;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(router);
 
-app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+if (cluster.isMaster) {
+    for (let i = 0; i < cpu_length; i++) {
+        cluster.fork();
+    }
+} else {
+    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+}
